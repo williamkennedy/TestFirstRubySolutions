7bfa4fa29577a53d34cf9d2f24b42a15c781020c
williamkennedy <williamkennedyjnr@gmail.com>
2014-05-28 23:29:42 +0100

    update

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -5,6 +5,19 @@ def shout(word)
 	"hello".upcase
 	word.upcase
 end
-def repeat(word)
-	"#{word} #{word}"
+def repeat(word, num = 2)
+	 ([word] * num).join(" ")
+end
+def start_of_word(word, no)
+	word[0,no]
+	end
+
+def first_word(word)
+	word.split.first
+end
+def titleize(word)
+	words_no_cap = ["and", "or", "the", "over", "to", "the", "a", "but"]
+	s = word.split.map {|word| words_no_cap.include?(word)? word: word.capitalize!}
+	s[0] = s[0].capitalize
+	s.join(" ")
 end
